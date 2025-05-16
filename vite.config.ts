230c26e9807cbab66f41e4efc7f45acc23b24156
MuemarXe <muemarevans@gmail.com>
2025-05-16 08:18:10 +0300

    .

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,7 @@ export default defineConfig({
   ssr: {
     resolve: {
       conditions: ["node", "development|production"],
+      base: process.env.VITE_BASE_PATH || "/react-vite-deploy",
     },
   },
 });
